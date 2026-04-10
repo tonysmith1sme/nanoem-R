2432d441a660ffbdeb411663009d19fd2433858b
tonysmith1sme <tonysmith1sme@gmail.com>
2026-04-10 16:23:54 +0800

    [macOS] Remove unused spdlog environment configuration and lower logger level to debug

--- macos/main.mm
+++ macos/main.mm
@@ -18,7 +18,6 @@
 #include "CocoaThreadedApplicationService.h"
 #if defined(NANOEM_ENABLE_LOGGING)
 #include "spdlog/async.h"
-#include "spdlog/cfg/env.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 #endif /* NANOEM_ENABLE_LOGGING */
 
@@ -93,10 +92,9 @@ main(int argc, char *argv[])
             sinks.push_back(std::make_shared<spdlog::sinks::stderr_color_sink_mt>());
             auto logger = std::make_shared<spdlog::async_logger>(
                 "emapp", sinks.begin(), sinks.end(), spdlog::thread_pool(), spdlog::async_overflow_policy::block);
-            logger->set_level(spdlog::level::trace);
+            logger->set_level(spdlog::level::debug);
             logger->flush_on(spdlog::level::debug);
             spdlog::register_logger(logger);
-            spdlog::cfg::load_env_levels();
             logger->info("nanoem logger initialized");
             logger->flush();
 #endif /* NANOEM_ENABLE_LOGGING */
