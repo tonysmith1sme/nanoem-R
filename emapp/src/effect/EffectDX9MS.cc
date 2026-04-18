0d9ac16c7019874b499e3bef5bf8396e48e0667b
tonysmith1sme <tonysmith1sme@gmail.com>
2026-04-18 20:37:32 +0800

    [emapp] Refactor HLSL shader compilation and add support for Direct3D11 in EffectDX9MS

--- emapp/src/effect/EffectDX9MS.cc
+++ emapp/src/effect/EffectDX9MS.cc
@@ -14,6 +14,325 @@ namespace nanoem {
 namespace effect {
 namespace dx9ms {
 
+struct HLSLField {
+    HLSLField(const String &name, const String &semantic, int index)
+        : m_name(name)
+        , m_semantic(semantic)
+        , m_index(index)
+    {
+    }
+    String m_name;
+    String m_semantic;
+    int m_index;
+};
+
+static void
+appendIndexedSemantic(String &value, const char *base, int index)
+{
+    char buffer[64];
+    if (index > 0 || StringUtils::equals(base, "SV_Target")) {
+        StringUtils::format(buffer, sizeof(buffer), "%s%d", base, index);
+    }
+    else {
+        StringUtils::copyString(buffer, base, sizeof(buffer));
+    }
+    value.append(buffer);
+}
+
+static void
+appendField(String &value, const char *type, const HLSLField &field)
+{
+    value.append("    ");
+    value.append(type);
+    value.append(" ");
+    value.append(field.m_name);
+    value.append(" : ");
+    appendIndexedSemantic(value, field.m_semantic.c_str(), field.m_index);
+    value.append(";\n");
+}
+
+static void
+collectVertexInputFields(const Fx9__Effect__Dx9ms__Shader *shaderPtr, tinystl::vector<HLSLField, TinySTLAllocator> &fields)
+{
+    const size_t numAttributes = shaderPtr->n_attributes;
+    for (size_t i = 0; i < numAttributes; i++) {
+        const Fx9__Effect__Dx9ms__Attribute *attributePtr = shaderPtr->attributes[i];
+        switch (attributePtr->usage) {
+        case FX9__EFFECT__DX9MS__ATTRIBUTE_USAGE__AU_POSITION:
+            fields.push_back(HLSLField("a_position", "POSITION", 0));
+            break;
+        case FX9__EFFECT__DX9MS__ATTRIBUTE_USAGE__AU_NORMAL:
+            fields.push_back(HLSLField("a_normal", "NORMAL", 0));
+            break;
+        case FX9__EFFECT__DX9MS__ATTRIBUTE_USAGE__AU_TEXCOORD: {
+            char buffer[32];
+            StringUtils::format(buffer, sizeof(buffer), "a_texcoord%d", attributePtr->index);
+            fields.push_back(HLSLField(buffer, "TEXCOORD", attributePtr->index));
+            break;
+        }
+        case FX9__EFFECT__DX9MS__ATTRIBUTE_USAGE__AU_COLOR: {
+            char buffer[32];
+            StringUtils::format(buffer, sizeof(buffer), "a_color%d", attributePtr->index);
+            fields.push_back(HLSLField(buffer, "COLOR", attributePtr->index));
+            break;
+        }
+        case FX9__EFFECT__DX9MS__ATTRIBUTE_USAGE__AU_BLENDWEIGHT:
+            fields.push_back(HLSLField("a_weight", "BLENDWEIGHT", 0));
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+static void
+collectVertexOutputFields(const Fx9__Effect__Dx9ms__Shader *shaderPtr, tinystl::vector<HLSLField, TinySTLAllocator> &fields)
+{
+    fields.push_back(HLSLField("position", "SV_Position", 0));
+    const size_t numOutputs = shaderPtr->n_outputs;
+    for (size_t i = 0; i < numOutputs; i++) {
+        const Fx9__Effect__Dx9ms__Attribute *outputPtr = shaderPtr->outputs[i];
+        switch (outputPtr->usage) {
+        case FX9__EFFECT__DX9MS__ATTRIBUTE_USAGE__AU_TEXCOORD:
+            fields.push_back(HLSLField(outputPtr->name, "TEXCOORD", outputPtr->index));
+            break;
+        case FX9__EFFECT__DX9MS__ATTRIBUTE_USAGE__AU_COLOR:
+            fields.push_back(HLSLField(outputPtr->name, "COLOR", outputPtr->index));
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+static void
+collectPixelInputFields(const StringMap &shaderOutputVariables, tinystl::vector<HLSLField, TinySTLAllocator> &fields)
+{
+    StringSet processed;
+    for (StringMap::const_iterator it = shaderOutputVariables.begin(), end = shaderOutputVariables.end(); it != end; ++it) {
+        const String &mappedName = it->second;
+        if (processed.find(mappedName) != processed.end()) {
+            continue;
+        }
+        const String &source = it->first;
+        if (StringUtils::equals(source.c_str(), "gl_Color")) {
+            fields.push_back(HLSLField(mappedName, "COLOR", 0));
+        }
+        else if (StringUtils::equals(source.c_str(), "gl_SecondaryColor")) {
+            fields.push_back(HLSLField(mappedName, "COLOR", 1));
+        }
+        else if (StringUtils::hasPrefix(source.c_str(), "gl_TexCoord[")) {
+            const char *ptr = source.c_str() + 12;
+            int index = StringUtils::parseInteger(ptr, nullptr);
+            fields.push_back(HLSLField(mappedName, "TEXCOORD", index));
+        }
+        else if (StringUtils::equals(source.c_str(), "gl_TexCoord")) {
+            fields.push_back(HLSLField(mappedName, "TEXCOORD", 0));
+        }
+        processed.insert(mappedName);
+    }
+}
+
+static void
+appendSamplerDeclarations(const Fx9__Effect__Dx9ms__Shader *shaderPtr, String &value)
+{
+    for (size_t i = 0, numSamplers = shaderPtr->n_samplers; i < numSamplers; i++) {
+        const Fx9__Effect__Dx9ms__Sampler *samplerPtr = shaderPtr->samplers[i];
+        const nanoem_u32_t index = samplerPtr->index;
+        const char *textureType = "Texture2D";
+        switch (samplerPtr->type) {
+        case FX9__EFFECT__DX9MS__SAMPLER_TYPE__SAMPLER_CUBE:
+            textureType = "TextureCube";
+            break;
+        case FX9__EFFECT__DX9MS__SAMPLER_TYPE__SAMPLER_VOLUME:
+            textureType = "Texture3D";
+            break;
+        case FX9__EFFECT__DX9MS__SAMPLER_TYPE__SAMPLER_2D:
+        default:
+            break;
+        }
+        char buffer[128];
+        StringUtils::format(buffer, sizeof(buffer), "%s %s_texture : register(t%d);\n", textureType,
+            samplerPtr->name, index);
+        value.append(buffer);
+        StringUtils::format(buffer, sizeof(buffer), "SamplerState %s : register(s%d);\n", samplerPtr->name, index);
+        value.append(buffer);
+    }
+}
+
+static void
+appendUniformBufferDeclaration(const Fx9__Effect__Dx9ms__Shader *shaderPtr, const char *name, String &value)
+{
+    nanoem_u32_t maxUniformIndices = 0;
+    for (size_t i = 0, numUniforms = shaderPtr->n_uniforms; i < numUniforms; i++) {
+        const Fx9__Effect__Dx9ms__Uniform *uniformPtr = shaderPtr->uniforms[i];
+        maxUniformIndices = glm::max(maxUniformIndices, uniformPtr->index);
+    }
+    char buffer[128];
+    StringUtils::format(buffer, sizeof(buffer), "cbuffer %s_t : register(b0) { float4 %s[%d]; };\n", name, name,
+        maxUniformIndices + 1);
+    value.append(buffer);
+}
+
+static void
+replaceAllString(String &value, const char *from, const char *to)
+{
+    if (!from || !to) {
+        return;
+    }
+    String::size_type offset = 0;
+    const String source(from), dest(to);
+    while ((offset = value.find(source, offset)) != String::npos) {
+        value.replace(offset, source.size(), dest);
+        offset += dest.size();
+    }
+}
+
+static void
+appendCommonHLSLDefines(String &value)
+{
+    value.append("#define highp\n");
+    value.append("#define mediump\n");
+    value.append("#define middlep\n");
+    value.append("#define lowp\n");
+    value.append("#define vec2 float2\n");
+    value.append("#define vec3 float3\n");
+    value.append("#define vec4 float4\n");
+    value.append("#define ivec2 int2\n");
+    value.append("#define ivec3 int3\n");
+    value.append("#define ivec4 int4\n");
+    value.append("#define bvec2 bool2\n");
+    value.append("#define bvec3 bool3\n");
+    value.append("#define bvec4 bool4\n");
+    value.append("#define mat3 float3x3\n");
+    value.append("#define mat4 float4x4\n");
+    value.append("#define mix lerp\n");
+    value.append("#define fract frac\n");
+    value.append("#define mod fmod\n");
+    value.append("#define texture2D(s, v) s##_texture.Sample(s, (v).xy)\n");
+    value.append("#define texture2DLod(s, v) s##_texture.SampleLevel(s, (v).xy, (v).w)\n");
+}
+
+static void
+appendProcessedHLSLBody(const Fx9__Effect__Dx9ms__Shader *shaderPtr, bool isVertexShader, String &value)
+{
+    const size_t numUniforms = shaderPtr->n_uniforms;
+    static const char kUniformPrefix[] = "uniform vec4 ";
+    static const char kVersionPrefix[] = "#version ";
+    static const char kAttributePrefix[] = "attribute ";
+    static const char kVaryingPrefix[] = "varying ";
+    static const char kOutPrefix[] = "out vec4 ";
+    static const char kDefineVSCPrefix[] = "#define vs_c";
+    static const char kDefinePSCPrefix[] = "#define ps_c";
+    static const char kDefinePSVPrefix[] = "#define ps_v";
+    static const char kDefineVSOPrefix[] = "#define vs_o";
+    BX_UNUSED_1(numUniforms);
+    bx::LineReader reader(shaderPtr->code);
+    int braceDepth = 0;
+    bool insertedReturn = false;
+    while (!reader.isDone()) {
+        const bx::StringView line = reader.next();
+        const char *linePtr = line.getPtr();
+        String current(linePtr, line.getTerm());
+        if (current.empty() || *current.c_str() == '\r') {
+            continue;
+        }
+        if (StringUtils::hasPrefix(linePtr, kVersionPrefix) || StringUtils::hasPrefix(linePtr, kAttributePrefix) ||
+            StringUtils::hasPrefix(linePtr, kVaryingPrefix) || StringUtils::hasPrefix(linePtr, kOutPrefix) ||
+            StringUtils::hasPrefix(linePtr, kUniformPrefix)) {
+            continue;
+        }
+        if (StringUtils::hasPrefix(linePtr, kDefineVSOPrefix)) {
+            continue;
+        }
+        if (StringUtils::hasPrefix(linePtr, kDefineVSCPrefix) || StringUtils::hasPrefix(linePtr, kDefinePSCPrefix)) {
+            const bool isVS = StringUtils::hasPrefix(linePtr, kDefineVSCPrefix);
+            const bx::StringView basePtr(line, sizeof(kDefineVSCPrefix) - 1);
+            char *mutablePtr = nullptr;
+            int uniformIndex = StringUtils::parseInteger(basePtr.getPtr(), &mutablePtr);
+            const bx::StringView fromPtr = bx::strLTrimSpace(mutablePtr), toPtr = bx::strWord(fromPtr);
+            char variableName[64];
+            StringUtils::copyString(variableName, fromPtr.getPtr(),
+                glm::min(Inline::saturateInt32(sizeof(variableName)), toPtr.getLength()));
+            char buffer[128];
+            StringUtils::format(buffer, sizeof(buffer), "#define %s%d %s[%d]\n", isVS ? "vs_c" : "ps_c",
+                uniformIndex, isVS ? "vs_uniforms_vec" : "ps_uniforms_vec", uniformIndex);
+            value.append(buffer);
+            continue;
+        }
+        if (!isVertexShader && StringUtils::hasPrefix(linePtr, kDefinePSVPrefix)) {
+            char variableName[64], mappedName[64];
+            const bx::StringView basePtr(line, sizeof(kDefinePSCPrefix) - 1);
+            char *mutablePtr = nullptr;
+            int variableIndex = StringUtils::parseInteger(basePtr.getPtr(), &mutablePtr);
+            StringUtils::format(variableName, sizeof(variableName), "ps_v%d", variableIndex);
+            const bx::StringView fromPtr = bx::strLTrimSpace(mutablePtr);
+            const char *strPtr = fromPtr.getPtr();
+            while (strPtr != line.getTerm() &&
+                (bx::isAlphaNum(*strPtr) || *strPtr == '_' || *strPtr == '[' || *strPtr == ']')) {
+                strPtr++;
+            }
+            StringUtils::copyString(mappedName, fromPtr.getPtr(), size_t(strPtr - fromPtr.getPtr() + 1));
+            String fieldName;
+            if (StringUtils::equals(mappedName, "gl_Color")) {
+                fieldName = "input.v_color0";
+            }
+            else if (StringUtils::equals(mappedName, "gl_SecondaryColor")) {
+                fieldName = "input.v_color1";
+            }
+            else if (StringUtils::hasPrefix(mappedName, "gl_TexCoord[")) {
+                int index = StringUtils::parseInteger(mappedName + 12, nullptr);
+                char buffer[64];
+                StringUtils::format(buffer, sizeof(buffer), "input.v_texcoord%d", index);
+                fieldName = buffer;
+            }
+            else if (StringUtils::equals(mappedName, "gl_TexCoord")) {
+                fieldName = "input.v_texcoord0";
+            }
+            if (!fieldName.empty()) {
+                value.append("#define ");
+                value.append(variableName);
+                value.append(" ");
+                value.append(fieldName);
+                value.append("\n");
+            }
+            continue;
+        }
+        replaceAllString(current, "void main()", isVertexShader ? "VS_OUTPUT main(VS_INPUT input)" : "PS_OUTPUT main(PS_INPUT input)");
+        replaceAllString(current, "void main(void)", isVertexShader ? "VS_OUTPUT main(VS_INPUT input)" : "PS_OUTPUT main(PS_INPUT input)");
+        replaceAllString(current, "gl_FragColor", "output.o_color0");
+        replaceAllString(current, "gl_FragData", "output.o_color");
+        replaceAllString(current, "vec2", "float2");
+        replaceAllString(current, "vec3", "float3");
+        replaceAllString(current, "vec4", "float4");
+        replaceAllString(current, "mat3", "float3x3");
+        replaceAllString(current, "mat4", "float4x4");
+        replaceAllString(current, "fract", "frac");
+        replaceAllString(current, "mix", "lerp");
+        if (current.find("main(") != String::npos) {
+            value.append(current);
+            value.append("\n");
+            value.append(isVertexShader ? "    VS_OUTPUT output = (VS_OUTPUT) 0;\n" : "    PS_OUTPUT output = (PS_OUTPUT) 0;\n");
+            braceDepth = 1;
+            continue;
+        }
+        for (String::const_iterator it = current.begin(), end = current.end(); it != end; ++it) {
+            if (*it == '{') {
+                braceDepth++;
+            }
+            else if (*it == '}') {
+                if (braceDepth == 1 && !insertedReturn) {
+                    value.append("    return output;\n");
+                    insertedReturn = true;
+                }
+                braceDepth--;
+            }
+        }
+        value.append(current);
+        value.append("\n");
+    }
+}
+
 static inline bool
 findString(const bx::StringView &line, const char *outPixelCandidate, bx::StringView &p)
 {
@@ -312,6 +631,96 @@ createShader(const Fx9__Effect__Dx9ms__Shader *shaderPtr, sg_shader_desc &desc, 
             nanoem_assert(false, "must NOT reach here");
             break;
         }
+    }
+}
+
+void
+createHLSLShader(const Fx9__Effect__Dx9ms__Shader *shaderPtr, sg_shader_desc &desc, String &newShaderCode,
+    StringMap &shaderOutputVariables)
+{
+    if (!shaderPtr->code || bx::strLen(shaderPtr->code) == 0) {
+        return;
+    }
+    const bool isVertexShader = shaderPtr->type == FX9__EFFECT__DX9MS__SHADER_TYPE__ST_VERTEX;
+    tinystl::vector<HLSLField, TinySTLAllocator> fields;
+    appendCommonHLSLDefines(newShaderCode);
+    if (isVertexShader) {
+        collectVertexInputFields(shaderPtr, fields);
+        newShaderCode.append("struct VS_INPUT {\n");
+        for (size_t i = 0; i < fields.size(); i++) {
+            appendField(newShaderCode, "float4", fields[i]);
+        }
+        newShaderCode.append("};\n");
+        fields.clear();
+        collectVertexOutputFields(shaderPtr, fields);
+        newShaderCode.append("struct VS_OUTPUT {\n");
+        for (size_t i = 0; i < fields.size(); i++) {
+            appendField(newShaderCode, "float4", fields[i]);
+        }
+        newShaderCode.append("};\n");
+        appendUniformBufferDeclaration(shaderPtr, "vs_uniforms_vec", newShaderCode);
+        appendSamplerDeclarations(shaderPtr, newShaderCode);
+        newShaderCode.append("#define vs_o0 output.position\n");
+        const size_t numAttributes = shaderPtr->n_attributes;
+        for (size_t i = 0; i < numAttributes; i++) {
+            const Fx9__Effect__Dx9ms__Attribute *attributePtr = shaderPtr->attributes[i];
+            switch (attributePtr->usage) {
+            case FX9__EFFECT__DX9MS__ATTRIBUTE_USAGE__AU_POSITION:
+                newShaderCode.append("#define ");
+                newShaderCode.append(attributePtr->name);
+                newShaderCode.append(" input.a_position\n");
+                break;
+            case FX9__EFFECT__DX9MS__ATTRIBUTE_USAGE__AU_NORMAL:
+                newShaderCode.append("#define ");
+                newShaderCode.append(attributePtr->name);
+                newShaderCode.append(" input.a_normal\n");
+                break;
+            case FX9__EFFECT__DX9MS__ATTRIBUTE_USAGE__AU_TEXCOORD: {
+                char buffer[64];
+                StringUtils::format(buffer, sizeof(buffer), "#define %s input.a_texcoord%d\n", attributePtr->name,
+                    attributePtr->index);
+                newShaderCode.append(buffer);
+                break;
+            }
+            case FX9__EFFECT__DX9MS__ATTRIBUTE_USAGE__AU_COLOR: {
+                char buffer[64];
+                StringUtils::format(buffer, sizeof(buffer), "#define %s input.a_color%d\n", attributePtr->name,
+                    attributePtr->index);
+                newShaderCode.append(buffer);
+                break;
+            }
+            case FX9__EFFECT__DX9MS__ATTRIBUTE_USAGE__AU_BLENDWEIGHT:
+                newShaderCode.append("#define ");
+                newShaderCode.append(attributePtr->name);
+                newShaderCode.append(" input.a_weight\n");
+                break;
+            default:
+                break;
+            }
+        }
+        for (size_t i = 0, numOutputs = shaderPtr->n_outputs; i < numOutputs; i++) {
+            const Fx9__Effect__Dx9ms__Attribute *outputPtr = shaderPtr->outputs[i];
+            newShaderCode.append("#define ");
+            newShaderCode.append(outputPtr->name);
+            newShaderCode.append(" output.");
+            newShaderCode.append(outputPtr->name);
+            newShaderCode.append("\n");
+        }
+        appendProcessedHLSLBody(shaderPtr, true, newShaderCode);
+        desc.vs.source = newShaderCode.c_str();
+    }
+    else {
+        collectPixelInputFields(shaderOutputVariables, fields);
+        newShaderCode.append("struct PS_INPUT {\n");
+        for (size_t i = 0; i < fields.size(); i++) {
+            appendField(newShaderCode, "float4", fields[i]);
+        }
+        newShaderCode.append("};\n");
+        newShaderCode.append("struct PS_OUTPUT {\n    float4 o_color0 : SV_Target0;\n};\n");
+        appendUniformBufferDeclaration(shaderPtr, "ps_uniforms_vec", newShaderCode);
+        appendSamplerDeclarations(shaderPtr, newShaderCode);
+        appendProcessedHLSLBody(shaderPtr, false, newShaderCode);
+        desc.fs.source = newShaderCode.c_str();
     }
 }
 
