2082e5716121e7de4d1c69de1806dad88a6bc80b
tonysmith1sme <tonysmith1sme@gmail.com>
2026-07-04 11:52:50 +0800

    [emapp] Fix Chinese character rendering: always include full CJK glyph ranges, use Noto fonts as primary for Chinese UI

--- emapp/src/internal/ImGuiWindow.cc
+++ emapp/src/internal/ImGuiWindow.cc
@@ -1614,15 +1614,7 @@ ImGuiWindow::setFontPointSize(nanoem_f32_t pointSize)
         }
         else {
             builder.AddRanges(m_atlas.GetGlyphRangesCyrillic());
-            switch (language) {
-            case ITranslator::kLanguageTypeChineseSimplified:
-            case ITranslator::kLanguageTypeChineseTraditional:
-                builder.AddRanges(m_atlas.GetGlyphRangesChineseFull());
-                break;
-            default:
-                builder.AddRanges(m_atlas.GetGlyphRangesChineseSimplifiedCommon());
-                break;
-            }
+            builder.AddRanges(m_atlas.GetGlyphRangesChineseFull());
             builder.AddRanges(m_atlas.GetGlyphRangesJapanese());
             builder.AddRanges(m_atlas.GetGlyphRangesKorean());
             builder.AddRanges(m_atlas.GetGlyphRangesThai());
